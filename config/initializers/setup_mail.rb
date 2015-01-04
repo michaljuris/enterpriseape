ebd55571f38459e2189eccaba761651ec7647403
Michal Juriš <michaljuris@gmail.com>
2015-01-04 12:13:39 +0000

    Chenged sendgrig SMTP credentials to ENV variables for security

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
-  :user_name            =>  'app32922075@heroku.com',
-  :password             =>  'auuohrz8',
+  :user_name            =>  ENV['SENDGRID_USERNAME'],
+  :password             =>  ENV['SENDGRID_PASSWORD'],
   :domain               =>  'heroku.com',
   :enable_starttls_auto =>  true
   }
