7906e0eb6b7cc57d864fbd30fad1f3ea25ce85b3
Michal Juriš <michaljuris@gmail.com>
2015-01-05 18:00:29 +0000

    Added purchases, nested to invoices (controller/view/routes) purchases show in Invoices Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :employees
 
   resources :companies do
@@ -19,7 +20,9 @@ Rails.application.routes.draw do
 
   get 'welcome/features'
 
-  resources :invoices
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
+  end
 
   root to: 'welcome#index'
 
